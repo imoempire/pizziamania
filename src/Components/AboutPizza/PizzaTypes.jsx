53729de5a54ea74d50a8744e1987d0050c720b90
imoempire <mrimoempire@gmail.com>
2021-06-15 14:36:43 +0000

    add Pizzatype

---
+++ src/Components/AboutPizza/PizzaTypes.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import image3 from '../../images/B1.JPG'
+
+const AboutPizza = () => {
+    return (
+        <div className="container about-pizza">
+<div className="row"><h1 className="text-center">About Pizza</h1></div>
+    <div className="row">
+    <div className="col-md-6"><img src={image3} alt="pizza" className="img-responsive" /></div>
+        <div className="col-md-6">
+            <h1>Pizza is awesome</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                    Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                    natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit vero atque cupiditate. 
+                        Cumque tempore, nulla enim, consequatur praesentium recusandae at,
+                        natus ipsa sit officia assumenda odit consequuntur? Cumque, quod fugit?</p>              
+        </div>
+    </div>
+</div>
+    );
+}
+
+export default AboutPizza;
